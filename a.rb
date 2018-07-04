134c635de3258aa2bd5dedd7a8b975c188a9d724
Michael <micbarash@gmail.com>
2018-07-04 18:51:20 +0300

    Removed unnecessary stuff

--- a.rb
+++ a.rb
@@ -5,7 +5,7 @@ class Mastermind
 		@name = name
 		puts "Welcome to Mastermind, #{@name} and good luck!"
 		turns_remaining #here to initialize the amount of turns, otherwise it's nill
-		puts comp_choice #makes the computer choice
+		comp_choice #makes the computer choice
 	end
 
 	private
@@ -85,46 +85,7 @@ class Mastermind
 		puts "You lost :("
 		puts "The code was: #{comp_choice}"
 	end
-=begin	def user_guess(a, b, c, d) #this method is obviously not complete yet
-		@guess = {"a" => a, "b" => b, "c" => c, "d" => d}
-		if @guess == @cHash
-			puts "Holy fuck you guessed it on the first try! Congratulations!"
-		else
-			puts "Nah that's not right"
-			decrement_turn
-			display_turns	
-		end
-	end
-=end
 end
 
 my_game = Mastermind.new("Shmulik")
 my_game.play()
-
-=begin
-
-h1 = {"a" => 4, "b" => 2, "c" => 3, "d" => 6}
-
-h2 = {"a" => 4, "b" => 2, "c" => 2, "d" => 4}
-
-x = "a"
-full_match_keys = []
-while x < "e" 
-	h1.each do |key,value|
-		if key == x && value == h2[x]
-			match += 1
-			full_match_keys << key
-		elsif key != x && value == h2[x] 
-			if full_match_keys.include?(key)
-				next
-			else
-				half_match += 1
-			end
-		end
-	end
-	x = x.next
-end
-puts "found #{match} matches"
-puts "found #{half_match} half matches"
-p full_match_keys
-=end
