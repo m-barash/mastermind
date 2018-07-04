e1cd23ef3a3be4b2ef45c116144692b40be1ccfd
Michael <micbarash@gmail.com>
2018-07-04 18:48:53 +0300

    Complete main game feature

--- a.rb
+++ a.rb
@@ -10,7 +10,7 @@ class Mastermind
 
 	private
 	def turns_remaining
-		@turns_remaining = 3
+		@turns_remaining = 12
 	end
 
 	def decrement_turn
@@ -35,14 +35,37 @@ class Mastermind
 
 	private
 	def give_feedback
-		#variables are equivilant to colored peg and white peg
-		color_and_position = 0
-		color_only = 0
+		match = 0
+		half_match = 0
+		x = "a"
+		full_match_keys = []
+		half_match_keys = []
+		while x < "e" 
+			@guessH.each do |key,value|
+				if key == x && value == @cHash[x]
+					match += 1
+					full_match_keys << key
+				elsif key != x && value == @cHash[x] 
+					if full_match_keys.include?(key) || half_match_keys.include?(key)
+						next
+					else
+						half_match += 1
+						half_match_keys << key
+					end
+				end
+			end
+			x = x.next
+		end
+		puts "found #{match} matches"
+		puts "found #{half_match} half matches"
+		p full_match_keys
+		p half_match_keys
 	end
 
 	public
-	def play #this works but does not yet give feedback
+	def play 
 		while @turns_remaining > 0
+			puts "-----------------------"
 			display_turns
 			puts "enter your guess"
 			@user_guess = gets.chomp.downcase.split(" ")
@@ -52,13 +75,15 @@ class Mastermind
 			@guessH['c'] = @user_guess[2]
 			@guessH['d'] = @user_guess[3]
 			if @guessH == @cHash
-				puts "yay correct"
+				puts "You broke the code! Congratulations!"
 				break
 			else
-				puts "wrong buddy"
+				give_feedback
 				decrement_turn
 			end
 		end
+		puts "You lost :("
+		puts "The code was: #{comp_choice}"
 	end
 =begin	def user_guess(a, b, c, d) #this method is obviously not complete yet
 		@guess = {"a" => a, "b" => b, "c" => c, "d" => d}
@@ -75,3 +100,31 @@ end
 
 my_game = Mastermind.new("Shmulik")
 my_game.play()
+
+=begin
+
+h1 = {"a" => 4, "b" => 2, "c" => 3, "d" => 6}
+
+h2 = {"a" => 4, "b" => 2, "c" => 2, "d" => 4}
+
+x = "a"
+full_match_keys = []
+while x < "e" 
+	h1.each do |key,value|
+		if key == x && value == h2[x]
+			match += 1
+			full_match_keys << key
+		elsif key != x && value == h2[x] 
+			if full_match_keys.include?(key)
+				next
+			else
+				half_match += 1
+			end
+		end
+	end
+	x = x.next
+end
+puts "found #{match} matches"
+puts "found #{half_match} half matches"
+p full_match_keys
+=end
